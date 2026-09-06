5a3f118a0af67e431e45fae143a81a3a16c06612
John <johannesdejenu3@gmail.noreply.github.com>
2026-09-06 21:56:19 +0300

    working on the Library class

--- Library.java
+++ Library.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+
 public class Library{
-    
+    private String name;
+    private ArrayList<Book> books;
+    private ArrayList<Member> members;
+
+
+
+
+    static{
+        System.out.println("Library system starting...");
+    }
+
+    {
+        System.out.println("Library initialized.");
+    }
+
+
 }
