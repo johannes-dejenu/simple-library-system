696b89515885ae16814b9f9882dfc6ccf846615c
John <johannesdejenu3@gmail.noreply.github.com>
2026-09-07 21:39:05 +0300

    some minor issues

--- Library.java
+++ Library.java
@@ -10,10 +10,12 @@ public class Library{
 
     static{
         System.out.println("Library system starting...");
+
     }
 
     {
         System.out.println("Library initialized.");
+
     }
 
     public String getName(){
@@ -31,12 +33,17 @@ public class Library{
     }
 
     public void addBook(Book book){
-        System.out.println("adding book to the " + getName() + " Library." );
+        System.out.println("adding " + book.getInfo().title() +  " to the " + getName() + " Library." );
+        System.out.println();
         books.add(book);
+        book.setStatus(BookStatus.AVAILABLE);
         System.out.println("book added successfully.");
+        System.out.println();
     }
     public void registerMember(Member member){
-        System.out.println("registering member to the " + getName() + " Library.");
+        System.out.println("registering" + member.getName() + " to the " + getName() + " Library.");
+        members.add(member);
+        System.out.println(member.getName() + " is registered successfully.");
     }
     public void borrowBook(Book book){
         book.setStatus(BookStatus.BORROWED);
